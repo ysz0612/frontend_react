91a5abba9d1400e578bf8a024b48cea827a55505
ysz0612 <ysjphj4344@gmail.com>
2026-05-13 17:41:20 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+const scores = [
+  { 이름: 'John', 국어: 90, 영어: 90, 수학: 80, 과학:90},
+  { 이름: 'Peter', 국어: 90, 영어: 90, 수학: 80, 과학:90},
+  { 이름: 'Susan', 국어: 90, 영어: 90, 수학: 80, 과학:90},
+  { 이름: 'Sue', 국어: 90, 영어: 90, 수학: 80, 과학:90}
+]
+// let numbers = [ 0, 1, 2, 3]
+
+// let newScores = numbers.map(item=>{return item*2})
+// console.log(newScores)
+
+// let newScores = []
+// for(let i=0;i<numbers.length;i++){
+//   newScores.push(numbers[i]*2)
+// }   //이게 매핑작업
+
+// {console.log(newScores)}
+
+function App() {
+
+
+  return (
+
+    <table>
+      
+      <tr>
+        <th>이름</th>
+        <th>국어</th>
+        <th>영어</th>
+        <th>수학</th>
+        <th>과학</th>
+      </tr>
+      {[0, 1, 2, 3].map(idx => 
+        (
+          <tr>
+          <td>{scores[0].이름}</td>,
+          <td>{scores[0].국어}</td>,
+          <td>{scores[0].영어}</td>,
+          <td>{scores[0].수학}</td>,
+          <td>{scores[0].과학}</td>
+        </tr>
+      )
+}
+    </table>
+  )
+}
+
+export default App
